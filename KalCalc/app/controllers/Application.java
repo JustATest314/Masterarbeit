99624c5497c069a09cd358432270d654affe01f9
JustATest314 <hale_chan@web.de>
2015-04-04 18:27:24 +0200

    Radiobuttons für Geschlecht hinzugefügt, berechnet nun korrekt anhand des Geschlechts die Grundumsatz-Werte. Ausserdem Kosmetik und aufgeräumt

--- KalCalc/app/controllers/Application.java
+++ KalCalc/app/controllers/Application.java
@@ -5,22 +5,42 @@ import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import play.Logger;
+
 public class Application extends Controller {
 
 	final static Form<User> userForm = Form.form(User.class);
-	
+
 	public static Result index() {
 		return ok(views.html.index.render(userForm));
 	}
 
-	public static Result submit(){
-//		Harris Benedict Formel fuer Grundumsatz
-//		66,47 + (13,7 * Körpergewicht [kg]) + (5 * Körpergröße [cm]) – (6,8 * Alter [Jahre])
+	public static Result submit() {
+		// Harris Benedict Formel fuer Grundumsatz (Mann)
+		// 66,47 + (13,7 * Körpergewicht [kg]) + (5 * Körpergröße [cm]) – (6,8 *
+		// Alter [Jahre])
+
+		// Formel fuer Frauen
+		// 655,1 + (9,6 * Körpergewicht [kg]) + (1,8 * Körpergröße [cm]) – (4,7
+		// * Alter [Jahre])
 		Form<User> filledForm = userForm.bindFromRequest();
 		User created = filledForm.get();
-		created.grundUmsatz = (float) (66.47 + (13.7 * created.gewicht) + (5 * created.groesse) - (6.8 * created.alter));
+
+		// For debug
+		 Logger.info(created.toString());
+
+		if (created.geschlecht.equals("Mann")) {
+			created.grundUmsatz = (float) (66.47 + (13.7 * created.gewicht)
+					+ (5 * created.groesse) - (6.8 * created.alter));
+		}
+
+		else if (created.geschlecht.equals("Frau")) {
+			created.grundUmsatz = (float) (655.1 + (9.6 * created.gewicht)
+					+ (1.8 * created.groesse) - (4.7 * created.alter));
+		}
 		return ok(views.html.submit.render(created));
+		// For debug only
+		// return status(488, "Strange response type");
 	}
 
-	
 }
--- KalCalc/app/models/User.java
+++ KalCalc/app/models/User.java
@@ -5,5 +5,14 @@ public class User {
 	public Integer groesse;
 	public Integer alter;
 
+	public String geschlecht;
+
 	public Float grundUmsatz;
+
+	public String toString() {
+		 return "Gewicht " + this.gewicht + " Groesse " + this.groesse +
+		 " Alter " + this.alter + " GU " + this.grundUmsatz + " Geschlecht " +
+		 this.geschlecht;
+	
+	}
 }

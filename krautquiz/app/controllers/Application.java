5fd81828f087b26d7df1a7186849c9ab0ce9116c
JustATest314 <hale_chan@web.de>
2015-04-26 16:40:44 +0200

    Startseite enthält nun generische Erzeugung von Fragen / Antworten. TODO: Fragen und Antworten zu Pärchen verknüpfen

--- krautquiz/app/controllers/Application.java
+++ krautquiz/app/controllers/Application.java
@@ -9,27 +9,38 @@ import views.html.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
+	static List<String> listOfQuestions = new ArrayList<String>();
+	static List<String> listOfAnswers = new ArrayList<String>();
+	
+	// Hilfsmethode, um Daten während der Entwicklung zu mocken
+	public static void QAinitialisieren(){
+		
+		// Listen löschen, da die Elemente angehängt werden, gäbe sonst doppelte Einträge
+		listOfQuestions.clear();
+		listOfAnswers.clear();
+		
+    	//Fragen
+    	listOfQuestions.add("Was passiert, wenn man ein 'break' verwendet?");
+    	listOfQuestions.add("Wieviele Fälle darf ein switch-case haben?");
+    	listOfQuestions.add("Geht das auch mit liste.clear()?");
+    	
+    	//Antworten
+    	listOfAnswers.add("Die Schleife springt einfach durch");
+    	listOfAnswers.add("Das Programm hängt sich auf");
+    	listOfAnswers.add("Ich weiss es selbst nicht...");
+    }
+
+	public static Result index() {
+    	QAinitialisieren();
+        return ok(index.render(listOfQuestions, listOfAnswers));
     }
 
     public static Result quizStarten(){
-    	
-    	// Fragen
-    	List<String> listOfQuestions = new ArrayList<String>();
-    	listOfQuestions.add("Was passiert, wenn man ein 'break' verwendet?");
-
-    	// Antworten
-    	List<String> listOfAnswers = new ArrayList<String>();
-    	listOfAnswers.add("Die Schleife springt einfach durch");
-    	listOfAnswers.add("Das Programm hängt sich auf");
-    	listOfAnswers.add("Ich weiss es auch nicht...");
-    	
+    	QAinitialisieren();
     	return ok(views.html.quiz.render(listOfQuestions, listOfAnswers));
     }
     
     public static Result zeigeEinstellungen(){
     	return ok(views.html.einstellungen.render());
     }
-    
 }

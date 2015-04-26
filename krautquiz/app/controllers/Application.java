6ce9d4c1388928cabbb1afaff4e2a6aa1e95056c
JustATest314 <hale_chan@web.de>
2015-04-26 16:07:38 +0200

    Scala-Template in Krautquiz eingeführt. Quiz wird jetzt über generisches Template erstellt.
    !!!PDF auf der ersten Seite anzeigen für Entwicklung deaktiviert ->
    RE-Aktivieren!!!

--- krautquiz/app/controllers/Application.java
+++ krautquiz/app/controllers/Application.java
@@ -1,8 +1,10 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
@@ -12,7 +14,18 @@ public class Application extends Controller {
     }
 
     public static Result quizStarten(){
-    	return ok(views.html.quiz.render());
+    	
+    	// Fragen
+    	List<String> listOfQuestions = new ArrayList<String>();
+    	listOfQuestions.add("Was passiert, wenn man ein 'break' verwendet?");
+
+    	// Antworten
+    	List<String> listOfAnswers = new ArrayList<String>();
+    	listOfAnswers.add("Die Schleife springt einfach durch");
+    	listOfAnswers.add("Das Programm hängt sich auf");
+    	listOfAnswers.add("Ich weiss es auch nicht...");
+    	
+    	return ok(views.html.quiz.render(listOfQuestions, listOfAnswers));
     }
     
     public static Result zeigeEinstellungen(){

307aaeacf9ffd469c285561d1139ef13d9a87508
JustATest314 <hamena314@freenet.de>
2015-04-28 11:09:24 +0200

    Datenstruktur Multimap probiert, wird wohl für QAs benutzt. Ausserdem UUID probiert, kann man gut für die IDs verwenden.

--- krautquiz/app/controllers/Application.java
+++ krautquiz/app/controllers/Application.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
@@ -15,12 +16,18 @@ public class Application extends Controller {
 	static List<String> listOfQuestions = new ArrayList<String>();
 	static List<String> listOfAnswers = new ArrayList<String>();
 
+	
+	  private static void log(Object aObject){
+		    System.out.println( String.valueOf(aObject) );
+		  }
+	
 	// Hilfsmethode, um Daten während der Entwicklung zu mocken
 
 	// TODOH Müsste umgestellt werden auf Array? Irgendwas, wo man Fragen, Scores
 	// und dazu gehörende Antworten mit Scores reintun kann
 	public static void QAinitialisieren() {
 		
+		// Multimap von Guava Google eingefügt, wird vermutlich die neue Datenstruktur
 		Multimap<String,String> myMultimap = ArrayListMultimap.create();
 
 		myMultimap.put("Frage 1", "id = sd");
@@ -28,9 +35,36 @@ public class Application extends Controller {
 		myMultimap.put("Frage 1", "vote = 100");
 		myMultimap.put("Frage 2", "id = ds");
 		
+		System.out.println("-------------");
+		
 		for(String key : myMultimap.keys()) {
 			Logger.info(key + myMultimap.values());
 		}
+		
+		myMultimap.remove("Frage 1", "vote = 100");
+		myMultimap.put("Frage 1", "vote = 200");
+		
+		System.out.println("-------------");
+		
+		for(String key : myMultimap.keys()) {
+			Logger.info(key + myMultimap.values());
+		}
+		
+		List<String> test = new ArrayList<String>();
+		Logger.info(myMultimap.get("Frage 1").toString());
+		test.addAll(myMultimap.get("Frage 1"));
+		Logger.info("Testliste: " + test.toString());
+		
+		if(myMultimap.containsValue("vote = 200")){
+			Logger.info("Contains is true");
+		}
+		
+		// So generiert man eindeutige IDs, können für die QA-IDs benutzt werden
+		UUID idOne = UUID.randomUUID();
+	    UUID idTwo = UUID.randomUUID();
+	    log("UUID One: " + idOne);
+	    log("UUID Two: " + idTwo);
+		
 		
 		// Listen löschen, da die Elemente angehängt werden, gäbe sonst doppelte
 		// Einträge

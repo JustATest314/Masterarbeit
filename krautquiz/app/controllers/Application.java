fcc3404944172cbc05fae08d6107430190fc197c
JustATest314 <hale_chan@web.de>
2015-05-07 21:41:05 +0200

    Fehler lokalisiert -> TODO Beheben! Geht um die doppelte Einfügung der Map, kommt daher, weil die Route falscherweise 2 mal aufgerufen wird.

--- krautquiz/app/controllers/Application.java
+++ krautquiz/app/controllers/Application.java
@@ -19,12 +19,6 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-//	static List<String> listOfQuestions = new ArrayList<String>();
-//	static List<String> listOfAnswers = new ArrayList<String>();
-	
-//	static List<String> questionList = new ArrayList<String>();
-//	static List<String> answerList = new ArrayList<String>();
-	
 	// HashMap for q/a, answers need to be linked somehow to questions, therefore List is used
 	// TODOL HashMap does no sorting i.e. of the voteScore -> Linked HashMap?
 	static Map<Question, List<Answer>> myMap = new HashMap<Question, List<Answer>>();
@@ -33,140 +27,6 @@ public class Application extends Controller {
 
 	// Helper-method for initializing the index page
 	public static void initialize() {
-		
-//		Multimap<String, String> questionMap = ArrayListMultimap.create();
-//		Multimap<String, String> answerMap = ArrayListMultimap.create();
-//		
-//		// Question in the format: Questiontext / ID / votes / UID
-//		// Question 1
-//		questionMap.put("What happens if I use a break here?", "e77dccbc-fd8d-4641-b9ca-17528e5d56b2");
-//		questionMap.put("What happens if I use a break here?", "150");
-//		questionMap.put("What happens if I use a break here?", "Marcus");
-//		
-//		// Answer in the format: Answertext / ID / Question-ID (Answer needs to be linked to a question) / votes / UID
-//		// Answer 1.1
-//		answerMap.put("The loop will just fall through!", "b8756ff5-ff8a-4a17-9517-811b91639fdf");
-//		answerMap.put("The loop will just fall through!", "e77dccbc-fd8d-4641-b9ca-17528e5d56b2");
-//		answerMap.put("The loop will just fall through!", "46");
-//		answerMap.put("The loop will just fall through!", "Lothar");
-//	
-//		// Question 2
-//		questionMap.put("How many cases must a switchcase have?", "e77dccbc-fd8d-4641-b9ca-17528e5d56b3");
-//		questionMap.put("How many cases must a switchcase have?", "34");
-//		questionMap.put("How many cases must a switchcase have?", "Frank");
-//		
-//		// Answer in the format: Answertext / ID / Question-ID (Answer needs to be linked to a question) / votes / UID
-//		// Answer 2.1
-//		answerMap.put("As many as you want!", "b8756ff5-ff8a-4a17-9517-811b91639fdg");
-//		answerMap.put("As many as you want!", "e77dccbc-fd8d-4641-b9ca-17528e5d56b3");
-//		answerMap.put("As many as you want!", "12");
-//		answerMap.put("As many as you want!", "Lothar");
-//		
-//		// Answer 2.2
-//		answerMap.put("None!", "b8756ff5-ff8a-4a17-9517-811b91639fdh");
-//		answerMap.put("None!", "e77dccbc-fd8d-4641-b9ca-17528e5d56b3");
-//		answerMap.put("None!", "9");
-//		answerMap.put("None!", "Steffi");
-//		
-//		// Question 3
-//		questionMap.put("Will this work with list.clear()?", "e77dccbc-fd8d-4641-b9ca-17528e5d56b4");
-//		questionMap.put("Will this work with list.clear()?", "92");
-//		questionMap.put("Will this work with list.clear()?", "Stefan");
-//		
-//		// Answer in the format: Answertext / ID / Question-ID (Answer needs to be linked to a question) / votes / UID
-//		// Answer 3.1
-//		answerMap.put("Sure!", "b8756ff5-ff8a-4a17-9517-811b91639fdi");
-//		answerMap.put("Sure!", "e77dccbc-fd8d-4641-b9ca-17528e5d56b4");
-//		answerMap.put("Sure!", "2");
-//		answerMap.put("Sure!", "Maria");
-		
-//		ListMultimap<String,String> myMultimap = ArrayListMultimap.create();
-//		 
-//		myMultimap.put("Fruits", "Bannana");
-//		myMultimap.put("Fruits", "Apple");
-//		myMultimap.put("Fruits", "Pear");
-//		myMultimap.put("Vegetables", "Carrot");
-//		
-//		List<String> myValues = myMultimap.get("Fruits");
-//		System.out.println("MyValues: " + myValues.toString());
-		
-//		Logger.info("KeySet: " + myMultimap.keySet().toString());
-//		Logger.info("Keys: " + myMultimap.keys().toString());
-		
-//		System.out.println("answermap.keySet(): ");
-//		Logger.info(answerMap.keySet().toString());
-		
-//		System.out.println("for over answerMap.values(): ");
-//		for(String key : answerMap.keys()) {
-//			Logger.info(key + answerMap.values() + "\n");
-//		}
-		
-//		// both lists contain only the keySets (questions / answers at the moment)
-//		questionList.addAll(questionMap.keySet());
-//		answerList.addAll(answerMap.keySet());
-		
-// 		Logger.info(questionMap.keys().toString());
-		
-//		// Clear lists, as the elements get appended. Else there would be duplicates
-//		listOfQuestions.clear();
-//		listOfAnswers.clear();
-//
-//		// Questions
-//		listOfQuestions.add("Was passiert, wenn man ein 'break' verwendet?");
-//		listOfQuestions.add("Wieviele Fälle darf ein switch-case haben?");
-//		listOfQuestions.add("Geht das auch mit liste.clear()?");
-//
-//		// Answers
-//		listOfAnswers.add("Die Schleife springt einfach durch");
-//		listOfAnswers.add("Das Programm hängt sich auf");
-//		listOfAnswers.add("Ich weiss es selbst nicht...");
-		
-//		System.out.println("--- questionMap ---");
-//		Logger.info(questionMap.toString());
-//		System.out.println("--- answerMap ---");
-//		Logger.info(answerMap.toString());
-		
-//		// Multimap von Guava Google eingefügt, wird vermutlich die neue Datenstruktur
-//		Multimap<String,String> myMultimap = ArrayListMultimap.create();
-//
-//		myMultimap.put("Frage 1", "id = sd");
-//		myMultimap.put("Frage 1", "qtext = dsf");
-//		myMultimap.put("Frage 1", "vote = 100");
-//		myMultimap.put("Frage 2", "id = ds");
-//		myMultimap.putAll("Frage 1", listOfAnswers);
-//		
-//		Logger.info(myMultimap.toString());
-//		
-//		System.out.println("-------------");
-//		
-//		for(String key : myMultimap.keys()) {
-//			Logger.info(key + myMultimap.values());
-//		}
-//		
-//		myMultimap.remove("Frage 1", "vote = 100");
-//		myMultimap.put("Frage 1", "vote = 200");
-//		
-//		System.out.println("-------------");
-//		
-//		for(String key : myMultimap.keys()) {
-//			Logger.info(key + myMultimap.values());
-//		}
-//		
-//		List<String> test = new ArrayList<String>();
-//		Logger.info(myMultimap.get("Frage 1").toString());
-//		test.addAll(myMultimap.get("Frage 1"));
-//		Logger.info("Testliste: " + test.toString());
-//		
-//		if(myMultimap.containsValue("vote = 200")){
-//			Logger.info("Contains is true");
-//		}
-//		
-//		// So generiert man eindeutige IDs, können für die QA-IDs benutzt werden
-//		UUID idOne = UUID.randomUUID();
-//	    UUID idTwo = UUID.randomUUID();
-//	    log("UUID One: " + idOne);
-//	    log("UUID Two: " + idTwo);
-		
 		// Question format: ID / questionText / voteScore / userID
 		// Answer format: ID / questionID (answer linked to question) / answerText / voteScore / userID
 		Question question1 = new Question("xyz", "Do Androids dream?", 127, "Marcus");
@@ -196,49 +56,28 @@ public class Application extends Controller {
 		answerList3.add(answer32);
 		answerList3.add(answer33);
 		
-//		Map<Question, List<Answer>> questionHashMap = new HashMap<Question, List<Answer>>();
-//		questionHashMap.put(frage1, antwortenListe1);
-//		questionHashMap.put(frage2, antwortenListe2);
-		
-//		for ( Question elem : questionHashMap.keySet() ){
-//			  System.out.println("Q-HashMap: " + elem.toString() );
-//			  for (List<Answer> elem2 : questionHashMap.values()){
-//
-//			  }
-//		}
-		
+
 		// clear(), else everytime the index page is called the q/as get added again
 //		myMap.clear();
 		myMap.put(question1, answerList1);
 		myMap.put(question2, answerList2);
 		myMap.put(question3, answerList3);
-		
-//		for(Question key : myMultimap.keys()) {
-//			System.out.println(key + myMultimap.get(key).toString());
-//		}
-//		
-//		System.out.println("===================");
-//		System.out.println(myMultimap.toString());
 		}
 
 	// Frontpage
 	public static Result index() {
 		initialize();
 		return ok(views.html.index.render(myMap));
-//		return ok(index.render(listOfQuestions, listOfAnswers));
 	}
 
 	// Quizpage
 	public static Result startQuiz() {
 		initialize();
 		return ok(views.html.quiz.render(myMap));
-//		return ok(views.html.quiz.render(listOfQuestions, listOfAnswers));
 	}
 
 	// Settings
 	public static Result showSettings() {
-		System.out.println("settings");
-		Logger.info("settings"); 
 		return ok(views.html.einstellungen.render());
 	}
 	
@@ -249,6 +88,8 @@ public class Application extends Controller {
 //		return ok(views.html.frageAntwort.render(myMap));
 		Form<Question> boundQuestion = newQuestion.bindFromRequest();
 		Question testQuestion = boundQuestion.get();
+		
+		System.out.println("boundQuestion: " + boundQuestion.toString());
 		
 		Answer answer51 = new Answer("adsf", "ewr", "SendMap Answer Form1!", 6, "Kittie");	
 		List<Answer> answerList5 = new ArrayList<Answer>();
@@ -261,9 +102,6 @@ public class Application extends Controller {
 	// In the frageAntwort-view a question gets asked, then the user gets transferred back to the index-page, where
 	// a fake question gets added
 	public static Result sendMap(){
-		
-		
-		
 		Question question4 = new Question("ertw", "Sendmap Question1", 34, "Tim");
 		Answer answer41 = new Answer("werw", "ertw", "SendMap Answer 1!", 12, "Oliver");
 		Answer answer42 = new Answer("tzsdfu", "ertw", "SendMap Answer 2!", 1, "Marcus");

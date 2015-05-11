ca0ecf952d2dff4bb32e5f9331305a5f723aa00e
JustATest314 <hamena314@freenet.de>
2015-05-11 11:46:55 +0200

    Map wurde komplett ausgebaut, läuft jetzt alles über Listen. Diese werden in die DB gespeichert.

--- krautquiz/app/controllers/Application.java
+++ krautquiz/app/controllers/Application.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 import com.avaje.ebean.ExpressionList;
 
@@ -21,71 +22,92 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 
-	// HashMap for q/a, answers need to be linked somehow to questions, therefore List is used
-	// TODOL HashMap does no sorting i.e. of the voteScore -> Linked HashMap?
-	static Map<Question, List<Answer>> myMap = new HashMap<Question, List<Answer>>();
+	// ArrayList might not be performant when there are many add / delete operations.
+	// If need be -> use LinkedList!
+	// TODOH Write methods for sorting the lists by voteScore
+	static List<Question> questionListAll = new ArrayList<Question>();
+	static List<Answer> answerListAll = new ArrayList<Answer>();
 	
 	private static final Form<Question> newQuestion = Form.form(Question.class);
-
+	
+	public static String generateFakeID(){
+		String fakeID = UUID.randomUUID().toString();
+		return fakeID;
+	}
+	
 	// Helper-method for initializing the index page
 	// TODOH Put this into a database!
 	public static void initialize() {
+		
 		// Question format: ID / questionText / voteScore / userID
 		// Answer format: ID / questionID (answer linked to question) / answerText / voteScore / userID
-		Question question1 = new Question("xyz", "Do Androids dream?", 127, "Marcus");
-		Answer answer11 = new Answer("zab", "xyz", "Only of electric sheep!", 70, "Tibor");
-		Answer answer12 = new Answer("qwert", "xyz", "No, they dont!", 10, "Sarah");
+		
+		// Fake IDs are needed while developing, so there are no duplicates
+		// TODO Remove in production
+		String questionFakeID1 = generateFakeID();
+		
+		Question question1 = new Question(questionFakeID1, "Do Androids dream?", 127, "Marcus");
+		Answer answer11 = new Answer(generateFakeID(), questionFakeID1, "Only of electric sheep!", 70, "Tibor");
+		Answer answer12 = new Answer(generateFakeID(), questionFakeID1, "No, they dont!", 10, "Sarah");
+		
+		String questionFakeID2 = generateFakeID();
 
-		Question question2 = new Question("fgh", "Why is the sky blue?", 76, "Frank");
-		Answer answer21 = new Answer("xcv", "fgh", "Frequency filtered sunlight!", 45, "Oliver");
-		Answer answer22 = new Answer("tzu", "fgh", "Light reflects from the blue sea water!", 3, "Tom");
+		Question question2 = new Question(questionFakeID2, "Why is the sky blue?", 76, "Frank");
+		Answer answer21 = new Answer(generateFakeID(), questionFakeID2, "Frequency filtered sunlight!", 45, "Oliver");
+		Answer answer22 = new Answer(generateFakeID(), questionFakeID2, "Light reflects from the blue sea water!", 3, "Tom");
 		
-		Question question3 = new Question("ertw", "How tall is tall?", 34, "Tim");
-		Answer answer31 = new Answer("werw", "ertw", "Depends on your definition!", 12, "Oliver");
-		Answer answer32 = new Answer("tzsdfu", "ertw", "Very!", 1, "Marcus");
-		Answer answer33 = new Answer("wcfewf", "ertw", "Not much!", 1, "Frank");
+		String questionFakeID3 = generateFakeID();
 		
-		// Put answers into a list and later into the HashMap
-		List<Answer> answerList1 = new ArrayList<Answer>();
-		answerList1.add(answer11);
-		answerList1.add(answer12);
+		Question question3 = new Question(questionFakeID3, "How tall is tall?", 34, "Tim");
+		Answer answer31 = new Answer(generateFakeID(), questionFakeID3, "Depends on your definition!", 12, "Oliver");
+		Answer answer32 = new Answer(generateFakeID(), questionFakeID3, "Very!", 1, "Marcus");
+		Answer answer33 = new Answer(generateFakeID(), questionFakeID3, "Not much!", 1, "Frank");
 		
-		List<Answer> answerList2 = new ArrayList<Answer>();
-		answerList2.add(answer21);
-		answerList2.add(answer22);
+		questionListAll.clear();
+		answerListAll.clear();
 		
-		List<Answer> answerList3 = new ArrayList<Answer>();
-		answerList3.add(answer31);
-		answerList3.add(answer32);
-		answerList3.add(answer33);
+		questionListAll.add(question1);
+		questionListAll.add(question2);
+		questionListAll.add(question3);
 		
-
-		// clear(), else everytime the index page is called the q/as get added again
-		myMap.clear();
-		myMap.put(question1, answerList1);
-		myMap.put(question2, answerList2);
-		myMap.put(question3, answerList3);
+		answerListAll.add(answer11);
+		answerListAll.add(answer12);
+		answerListAll.add(answer21);
+		answerListAll.add(answer22);
+		answerListAll.add(answer31);
+		answerListAll.add(answer32);
+		answerListAll.add(answer33);
+		
+		Question.create(question1);
+		Question.create(question2);
+		Question.create(question3);
+		
+		Answer.create(answer11);
+		Answer.create(answer12);
+		Answer.create(answer21);
+		Answer.create(answer22);
+		Answer.create(answer31);
+		Answer.create(answer32);
+		Answer.create(answer33);
 	}
 
 	// Frontpage
 	public static Result index() {
 		initialize();
 		
-		for (Question questionItem : Question.find.all()) {
-			System.out.println(questionItem.toString());
-		}
-		return ok(views.html.index.render(myMap));
+		// TODOH Get questions / answer from the DB, not manually
+		return ok(views.html.index.render(questionListAll, answerListAll));
 	}
 
 	// Quizpage
 	public static Result startQuiz() {
 		initialize();
-		return ok(views.html.quiz.render(myMap));
+		return ok(views.html.quiz.render(questionListAll, answerListAll));
 	}
 
 	// Settings
 	public static Result showSettings() {
-		return ok(views.html.einstellungen.render());
+		return ok(views.html.einstellungen.render(questionListAll, answerListAll));
 	}
 	
 	// Until now a new page gets generated where the user can enter a new question
@@ -95,21 +117,9 @@ public class Application extends Controller {
 		// Create new question-form and fill it with the values from the other page
 		Form<Question> boundQuestion = newQuestion.bindFromRequest();
 		Question testQuestion = boundQuestion.get();
-		
 		Question.create(testQuestion);
-		
-		// As an List[Answer] is used, create a fake answer. Cant be empty
-		Answer answer51 = new Answer("adsf", "ewr", "SendMap Answer Form1!", 6, "Kittie");	
-		Answer.create(answer51);
-		
-		List<Answer> answerList5 = new ArrayList<Answer>();
-		answerList5.add(answer51);
-		myMap.put(testQuestion, answerList5);
-		
-		Question tester = (Question) Question.find.where().eq("questionID" , "abcd").findUnique();
-		System.out.println("Tester: " + tester.toString());
-		
-		return ok(views.html.index.render(myMap));
+		questionListAll.add(testQuestion);
+		return ok(views.html.index.render(questionListAll, answerListAll));
 	}
 	
 	// Go to the ask question page
